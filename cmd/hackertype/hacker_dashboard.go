6ac8d0343e8a231bfee9e7d114a5e4851020eaad
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-03-23 14:32:13 -0400

    feat: ellipsis spinner

--- cmd/hackertype/hacker_dashboard.go
+++ cmd/hackertype/hacker_dashboard.go
@@ -7,25 +7,29 @@ import (
 )
 
 type HackerDashboard struct {
-	status  string
-	spinner spinner.Model
+	status       string
+	worldSpinner spinner.Model
+	ellipsis     spinner.Model
 }
 
 func NewHackerDashboard() HackerDashboard {
-	spinner := spinner.New(spinner.WithSpinner(spinner.Globe))
-	return HackerDashboard{status: "Hacking in progress...", spinner: spinner}
+	worldSpinner := spinner.New(spinner.WithSpinner(spinner.Globe))
+	ellipsis := spinner.New(spinner.WithSpinner(spinner.Ellipsis))
+	return HackerDashboard{status: "Hacking in progress", worldSpinner: worldSpinner, ellipsis: ellipsis}
 }
 
 func (hd HackerDashboard) Update(msg tea.Msg) (HackerDashboard, tea.Cmd) {
-	spinner, cmd := hd.spinner.Update(msg)
-	hd.spinner = spinner
-	return hd, cmd
+	worldSpinner, cmd1 := hd.worldSpinner.Update(msg)
+	hd.worldSpinner = worldSpinner
+	ellipsis, cmd2 := hd.ellipsis.Update(msg)
+	hd.ellipsis = ellipsis
+	return hd, tea.Batch(cmd1, cmd2)
 }
 
 func (hd HackerDashboard) View() string {
-	return lipgloss.JoinHorizontal(lipgloss.Top, hd.spinner.View(), hd.status)
+	return lipgloss.JoinHorizontal(lipgloss.Top, hd.worldSpinner.View(), hd.status, hd.ellipsis.View())
 }
 
 func (hd HackerDashboard) Init() tea.Cmd {
-	return hd.spinner.Tick
+	return tea.Batch(hd.worldSpinner.Tick, hd.ellipsis.Tick)
 }
